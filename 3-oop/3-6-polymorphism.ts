eae6bb2bdbeca63dad64062adeed7ad1bb92f14f
김지수 <kimjisu3817@gmail.com>
2022-07-17 20:41:43 +0900

    Feat: composition 예시시 코드 추가

--- 3-oop/3-6-polymorphism.ts
+++ 3-oop/3-6-polymorphism.ts
@@ -58,18 +58,13 @@
     }
   }
 
-  class LatteMachine extends CoffeeMachine {
-    constructor(coffeeBeans: number) {
-      super(coffeeBeans);
-    }
-
+  class CheapSteamer {
     private steamMilk() {
       console.log('milk in steam ... 🥛');
     }
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
-      this.steamMilk();
 
+    makeMilk(coffee: CoffeeCup): CoffeeCup {
+      this.steamMilk();
       return {
         ...coffee,
         hasMilk: true,
@@ -77,37 +72,68 @@
     }
   }
 
-  class SweetCoffeeMachine extends CoffeeMachine {
-    private addSugar() {
+  class AutomaticSugarMixer {
+    private getSugar() {
       console.log('add some sugar...');
+      return true;
+    }
+
+    addSugar(coffee: CoffeeCup): CoffeeCup {
+      const hasSugar = this.getSugar();
+      return {
+        ...coffee,
+        hasSugar,
+      };
+    }
+  }
+
+  class LatteMachine extends CoffeeMachine {
+    milkFrother: CheapSteamer;
+    constructor(coffeeBeans: number, milkFrother: CheapSteamer) {
+      super(coffeeBeans);
+      this.milkFrother = milkFrother;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.milkFrother.makeMilk(coffee);
+    }
+  }
+
+  class SweetCoffeeMachine extends CoffeeMachine {
+    sugar: AutomaticSugarMixer;
+    constructor(coffeeBeans: number, sugar: AutomaticSugarMixer) {
+      super(coffeeBeans);
+      this.sugar = sugar;
     }
 
     makeCoffee(shots: number): CoffeeCup {
       const coffee = super.makeCoffee(2);
-      this.addSugar();
-
-      return {
-        ...coffee,
-        hasSugar: true,
-      };
+      return this.sugar.addSugar(coffee);
     }
   }
-  const latteMachine = new LatteMachine(32);
+
+  const latteMachine = new LatteMachine(32, new CheapSteamer());
   const latte = latteMachine.makeCoffee(2);
 
-  const sweetCoffeeMachine = new SweetCoffeeMachine(32);
+  const sweetCoffeeMachine = new SweetCoffeeMachine(
+    32,
+    new AutomaticSugarMixer()
+  );
+
   const sweetCoffee = sweetCoffeeMachine.makeCoffee(2);
 
+  console.log(latte);
   console.log(sweetCoffee);
 
-  const machines: CoffeeMaker[] = [
-    new CoffeeMachine(32),
-    new LatteMachine(32),
-    new SweetCoffeeMachine(32),
-  ];
+  // const machines: CoffeeMaker[] = [
+  //   new CoffeeMachine(32),
+  //   new LatteMachine(32),
+  //   new SweetCoffeeMachine(32),
+  // ];
 
-  machines.forEach((machine) => {
-    console.log('---------------------------');
-    machine.makeCoffee(2);
-  });
+  // machines.forEach((machine) => {
+  //   console.log('---------------------------');
+  //   machine.makeCoffee(2);
+  // });
 }
