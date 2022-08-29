58ce5c6896786efc59e3fbd5d05dc7a14e58ae54
김지수 <kimjisu3817@gmail.com>
2022-08-29 23:21:43 +0900

    Add: Omit Type

---
+++ 9-advanced/9-8-omit.ts
@@ -0,0 +1,28 @@
+{
+}
+{
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+
+  type VideoMetaData = Omit<Video, 'url' | 'data' | 'h'>;
+
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: 'video',
+      url: 'http:// ...',
+      data: 'byte-data ...',
+    };
+  }
+
+  function getVideoMetaData(id: string): VideoMetaData {
+    return {
+      id: id,
+      title: 'title',
+    };
+  }
+}
